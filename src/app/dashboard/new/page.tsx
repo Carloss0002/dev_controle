f4d65d4a91d81e27322e063610a7a6e14eecacd0
Carloss0002 <carlosseduardo003@hotmail.com>
2025-05-05 21:13:49 -0300

    feat: terminada operação para enviar ticket para banco de dados

--- src/app/dashboard/new/page.tsx
+++ src/app/dashboard/new/page.tsx
@@ -17,6 +17,28 @@ export default async function NewTicket() {
       userId: session.user.id,
     },
   });
+
+  async function handleRegisterTicket(formData: FormData){
+    "use server"
+    const name = formData.get("name")
+    const description = formData.get("description")
+    const customer = formData.get("customer")
+    
+    if (!name || !description || !customer) return;
+
+    await prisma.ticket.create({
+      data: {
+        name: name as string,
+        description: description as string,
+        customerId: customer as string,
+        status: "ABERTO",
+        userId: session?.user.id
+      }
+    })
+
+    redirect("/dashboard")
+  }
+
   return (
     <Container>
       <main className="mt-9 mb-2">
@@ -29,13 +51,14 @@ export default async function NewTicket() {
           </Link>
           <h1 className="text-3xl font-bold">Novo chamados</h1>
         </div>
-        <form className="flex flex-col mt-6 ">
+        <form className="flex flex-col mt-6" action={handleRegisterTicket}>
           <label className="mb-1 font-medium text-lg">Nome do chamado</label>
           <input
             className="w-full border rounded-md px-2 mb-2 h-11"
             type="text"
             placeholder="Digite o nome do chamado"
             required
+            name="name"
           />
           <label className="mb-1 font-medium text-lg mt-4">
             Descreva o problema
@@ -44,13 +67,14 @@ export default async function NewTicket() {
             className="w-full border resize-none rounded-md px-2 mb-2 h-24"
             placeholder="Descreva o problema..."
             required
+            name="description"
           ></textarea>
           {customers.length !== 0 ? (
             <>
               <label className="mb-1 font-medium text-lg mt-4">
                 Selecione o problema
               </label>
-              <select className="w-full bg-white border rounded-md px-2 mb-2 h-11">
+              <select className="w-full bg-white border rounded-md px-2 mb-2 h-11"name="customer" >
                 {customers.map((customer) => (
                   <option value={customer.id}>{customer.name}</option>
                 ))}
